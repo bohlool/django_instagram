603c7190e42ed1cc0a5201fcb0f069783185e0c8
bohlool <bohlool.mohammadi.2009@gmail.com>
2024-03-03 22:40:45 +0330

    Add user creation logic to RegisterSerializer in user_profiles app.

--- user_profiles/serializers.py
+++ user_profiles/serializers.py
@@ -19,6 +19,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username', 'password', 'first_name', 'last_name')
 
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+        user = User.objects.create_user(username, password=password, first_name=first_name, last_name=last_name)
+        return user
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
