433e5cda3e9976347c209b14f8f5bd79c73c2fbc
bohlool <bohlool.mohammadi.2009@gmail.com>
2024-03-03 15:21:20 +0330

    Renamed object parameter to obj.

--- user_activities/utils.py
+++ user_activities/utils.py
@@ -3,17 +3,17 @@ from django.contrib.contenttypes.models import ContentType
 from .models import Like
 
 
-def track_like(object, user):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    Like.objects.get_or_create(content_type=content_type, object_id=object.id, content_object=object, user=user)
+def track_like(obj, user):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    Like.objects.get_or_create(content_type=content_type, object_id=obj.id, content_object=obj, user=user)
 
 
-def get_total_likes(object):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    return Like.objects.filter(content_type=content_type, object_id=object.id).count()
+def get_total_likes(obj):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return Like.objects.filter(content_type=content_type, object_id=obj.id).count()
 
 
-def get_enthusiasts(object):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    fans = Like.objects.filter(content_type=content_type, object_id=object.id).values_list('user', flat=True)
+def get_enthusiasts(obj):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    fans = Like.objects.filter(content_type=content_type, object_id=obj.id).values_list('user', flat=True)
     return fans
--- view_log/utils.py
+++ view_log/utils.py
@@ -3,22 +3,22 @@ from django.contrib.contenttypes.models import ContentType
 from .models import View
 
 
-def track_view(object, user):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    View.objects.create(content_type=content_type, object_id=object.id, content_object=object, user=user)
+def track_view(obj, user):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    View.objects.create(content_type=content_type, object_id=obj.id, content_object=obj, user=user)
 
 
-def get_total_views(object):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    return View.objects.filter(content_type=content_type, object_id=object.id).count()
+def get_total_views(obj):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return View.objects.filter(content_type=content_type, object_id=obj.id).count()
 
 
-def get_views_by_user(object, user):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    return View.objects.filter(content_type=content_type, object_id=object.id, user=user).count()
+def get_views_by_user(obj, user):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    return View.objects.filter(content_type=content_type, object_id=obj.id, user=user).count()
 
 
-def get_viewers(object):
-    content_type = ContentType.objects.get_for_model(object.__class__)
-    views = View.objects.filter(content_type=content_type, object_id=object.id).values_list('user', flat=True).distinct()
+def get_viewers(obj):
+    content_type = ContentType.objects.get_for_model(obj.__class__)
+    views = View.objects.filter(content_type=content_type, object_id=obj.id).values_list('user', flat=True).distinct()
     return views
