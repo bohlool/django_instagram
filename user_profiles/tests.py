147bbb414c4dd19f6e951abc5697cb308d9d3524
bohlool <bohlool.mohammadi.2009@gmail.com>
2024-03-04 18:41:29 +0330

    Add comprehensive user account tests for registration, login, and updates.

--- user_profiles/tests.py
+++ user_profiles/tests.py
@@ -1,1 +1,98 @@
-# Create your tests here.
+import django
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+User = get_user_model()
+
+
+class AccountTests(APITestCase):
+
+    def setUp(self):
+        self.user_data = {'username': 'liana', 'password': 'pass1234', 'first_name': 'Liana', 'last_name': 'Mohammadi'}
+
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('register-list')
+
+        response = self.client.post(url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(response.data), 4)
+
+    def test_login(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('token_obtain_pair')
+
+        User.objects.create_user(**self.user_data)
+        response = self.client.post(url,
+                                    {'username': self.user_data['username'], 'password': self.user_data['password']},
+                                    format='json')
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_change_password(self):
+        """
+        Ensure we can create a new account object.
+        """
+        user = User.objects.create_user(**self.user_data)
+
+        self.authorize_user(self.user_data['username'], self.user_data['password'])
+
+        url = reverse('change-pass-detail', args=[user.id])
+        new_password = 'p1234567'
+        response = self.client.put(url,
+                                   {'password': new_password, 'confirm_password': new_password},
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_change_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        user = User.objects.create_user(**self.user_data)
+
+        self.authorize_user(self.user_data['username'], self.user_data['password'])
+
+        url = reverse('change-account-detail', args=[user.id])
+        new_username = 'farnaz'
+        response = self.client.put(url,
+                                   {'username': new_username},
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 4)
+        new_user = User.objects.get(pk=user.id)
+        self.assertEqual(new_user.username, new_username)
+
+    def test_delete_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        user = User.objects.create_user(**self.user_data)
+
+        self.authorize_user(self.user_data['username'], self.user_data['password'])
+
+        url = reverse('change-account-detail', args=[user.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        try:
+            User.objects.get(pk=user.id)
+            self.fail("User is not deleted!")
+        except User.DoesNotExist:
+            pass
+
+    def authorize_user(self, username, password):
+        url = reverse('token_obtain_pair')
+        response = self.client.post(url,
+                                    {'username': username, 'password': password},
+                                    format='json')
+        if response.status_code != status.HTTP_200_OK:
+            return False
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + response.data['access'])
+        return True
